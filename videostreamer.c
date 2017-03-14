0b321843014f2b5a97372a8cf31245a219d169e8
Will Storey <will@summercat.com>
2017-03-14 11:10:56 -0700

    Fix indentation in a function.

--- videostreamer.c
+++ videostreamer.c
@@ -404,12 +404,11 @@ static void
 __vs_log_packet(const AVFormatContext * const format_ctx,
 		const AVPacket * const pkt, const char * const tag)
 {
-    AVRational * const time_base = &format_ctx->streams[pkt->stream_index]->time_base;
+		AVRational * const time_base = &format_ctx->streams[pkt->stream_index]->time_base;
 
-    printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
-           tag,
-           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
-           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
-           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
-           pkt->stream_index);
+		printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
+				tag, av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+				av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+				av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+				pkt->stream_index);
 }
